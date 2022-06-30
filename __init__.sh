cfbc75f6adbb46a55153ee9d47cdab51c34e2f92
CIRCERO <56322082+CIRCERO@users.noreply.github.com>
2022-06-30 23:08:22 +0200

    Initial Commit

---
+++ __init__.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/CIRCERO/Terra-Moon.git
+cd "#Terra-Moon"
+git init
+git add --all
+git commit -m "Initial Commit"
+git remote add origin https://github.com/CIRCERO/Terra-Moon.git
+git push -u origin master
+
---
+++ build.cmd
@@ -0,0 +1,9 @@
+echo "#Terra-Moon" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/CIRCERO/Terra-Moon.git
+git push -u origin main
+
+git remote add origin https://github.com/CIRCERO/CIRCERO/Terra-Moon.git
+git push -u origin main
